1323d44c4e0318bcc305d3996984f2dc7660a61b
Wendy <gwendolyn.monahan@gmail.com>
2019-12-12 02:21:12 -0800

    Initialized!

---
+++ src/components/layout/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Header.css';
+
+function Header() {
+  return (
+    <header className='headerStyle'>
+      <h1>To-Do  List</h1>
+    </header>
+  )
+}
+
+export default Header;
